0222e61efe14814402473d359a7babffc5dd232b
dkar1mov03 <dkarimov049@gmail.com>
2023-12-22 23:06:25 +0500

    Asset Done

--- src/Icarus.Service/DTOs/Assets/AssetForCreationDto.cs
+++ src/Icarus.Service/DTOs/Assets/AssetForCreationDto.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Icarus.Service.Helpers;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
 
 namespace Icarus.Service.DTOs.Assets;
 
@@ -6,12 +8,24 @@ public class AssetForCreationDto
 {
     public IFormFile Logo { get; set; }
     public decimal Rating { get; set; }
+
+    [PhoneAttribute]
     public string Phone { get; set; }
+
+    [IcarusEmailAttribute]
     public string Email { get; set; }
     public string Address { get; set; }
+
+    [UrlAttribute]
     public string CompanyWebUrl { get; set; }
+
+    [UrlAttribute]
     public string InstagramUrl { get; set; }
+
+    [UrlAttribute]
     public string TelegramUrl { get; set; }
+
+    [UrlAttribute]
     public string FacebookUrl { get; set; }
     public string Latitude { get; set; }
     public string Longitude { get; set; }
--- src/Icarus.Service/DTOs/Assets/AssetForUpdateDto.cs
+++ src/Icarus.Service/DTOs/Assets/AssetForUpdateDto.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Icarus.Service.Helpers;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
 
 namespace Icarus.Service.DTOs.Assets;
 
@@ -6,12 +8,23 @@ public class AssetForUpdateDto
 {
     public IFormFile Logo { get; set; }
     public decimal Rating { get; set; }
+    [PhoneAttribute]
     public string Phone { get; set; }
+
+    [IcarusEmailAttribute]
     public string Email { get; set; }
     public string Address { get; set; }
+
+    [UrlAttribute]
     public string CompanyWebUrl { get; set; }
+
+    [UrlAttribute]
     public string InstagramUrl { get; set; }
+
+    [UrlAttribute]
     public string TelegramUrl { get; set; }
+
+    [UrlAttribute]
     public string FacebookUrl { get; set; }
     public string Latitude { get; set; }
     public string Longitude { get; set; }
