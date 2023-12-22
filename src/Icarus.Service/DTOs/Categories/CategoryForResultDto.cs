cc81b330e50b45ce706e2fe3b501b342aafd8de6
Shamsiddin_Dev <119076053+Shamsiddin13@users.noreply.github.com>
2023-12-22 19:19:35 +0500

    Updated: Update many files

---
+++ src/Icarus.Service/DTOs/Categories/CategoryForResultDto.cs
@@ -0,0 +1,9 @@
+﻿using Icarus.Domain.Entities;
+
+namespace Icarus.Service.DTOs.Categories;
+
+public class CategoryForResultDto
+{
+    public string Name { get; set; }
+    public ICollection<DepartmentCategory> DepartmentCategories { get; set; }
+}
---
+++ src/Icarus.Service/DTOs/DepartmentCategories/DepartmentCategoryForResultDto.cs
@@ -0,0 +1,12 @@
+﻿using Icarus.Service.DTOs.Categories;
+using Icarus.Service.DTOs.Departments;
+
+namespace Icarus.Service.DTOs.DepartmentCategories;
+
+public class DepartmentCategoryForResultDto
+{
+    public DepartmentForResultDto Department { get; set; }
+    public CategoryForResultDto Category { get; set; }
+
+    public string Description { get; set; }
+}
