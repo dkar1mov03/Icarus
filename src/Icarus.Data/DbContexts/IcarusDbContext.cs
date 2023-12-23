a68cfd766279361457258a8ce29966e82f580a33
dkar1mov03 <dkarimov049@gmail.com>
2023-12-23 15:42:27 +0500

    Add: SeedData

--- src/Icarus.Data/DbContexts/IcarusDbContext.cs
+++ src/Icarus.Data/DbContexts/IcarusDbContext.cs
@@ -1,4 +1,7 @@
-﻿using Icarus.Domain.Entities;
+﻿using Icarus.Data.DbContexts.SeedDatas.AssetSeedDatas;
+using Icarus.Data.DbContexts.SeedDatas.DepartmentCategorySeedDatas;
+using Icarus.Data.DbContexts.SeedDatas.UserSeedDatas;
+using Icarus.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Icarus.Data.DbContexts;
@@ -13,20 +16,34 @@ public class IcarusDbContext : DbContext
     public DbSet<Category> Categories { get; set; }
     public DbSet<Department> Departments { get; set; }
     public DbSet<DepartmentCategory> DepartmentCategories { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {          
+    {
+        base.OnModelCreating(modelBuilder);
+
         // Category - DepartmentCategory Configuration
+
         modelBuilder.Entity<Category>()
             .HasMany(c => c.DepartmentCategories)
             .WithOne(dc => dc.Category)
             .HasForeignKey(dc => dc.CategoryId)
             .OnDelete(DeleteBehavior.Cascade);
-        
+
         // Department - DepartmentCategory Configuration
+
         modelBuilder.Entity<Department>()
             .HasMany(d => d.DepartmentCategories)
             .WithOne(dc => dc.Department)
             .HasForeignKey(dc => dc.DepartmentId)
             .OnDelete(DeleteBehavior.Cascade);
+
+           SeedData(modelBuilder);
+    }
+
+    private static void SeedData(ModelBuilder modelBuilder)
+    {
+        UserSeedData.SeedDataUsers(modelBuilder);
+        AssetSeetData.SeedDataAssets(modelBuilder);
+        DepartamentCategorySeedData.SeedDataDepartamentCategory(modelBuilder);
     }
 }
