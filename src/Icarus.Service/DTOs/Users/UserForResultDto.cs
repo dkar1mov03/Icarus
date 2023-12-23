b1800a8fe39a8da286d16a518ffc187146fc77bb
Khurshid Yuldoshev <xurshidyoldoshev4@gmail.com>
2023-12-23 09:55:59 +0500

    Password hasher added

--- src/Icarus.Service/DTOs/Users/UserForResultDto.cs
+++ src/Icarus.Service/DTOs/Users/UserForResultDto.cs
@@ -8,7 +8,7 @@ namespace Icarus.Service.DTOs.Users
         public string LastName { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
-        public string Image { get; set; }
         public string Password { get; set; }
+        public string Image { get; set; }
     }
 }
