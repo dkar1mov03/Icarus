d44dc9001bd4ad9fac49a608e38e85ffa261bb68
Ilhomjon-Azimov <ilhomjonazimov777@gmail.com>
2023-12-23 14:49:37 +0500

    Add: SeedData

--- src/Icarus.Data/DbContexts/IcarusDbContext.cs
+++ src/Icarus.Data/DbContexts/IcarusDbContext.cs
@@ -1,10 +1,13 @@
-﻿using Icarus.Domain.Entities;
+﻿using Icarus.Data.DbContexts.SeedDatas.AssetSeedDatas;
+using Icarus.Data.DbContexts.SeedDatas.DepartmentCategorySeedDatas;
+using Icarus.Data.DbContexts.SeedDatas.UserSeedDatas;
+using Icarus.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Icarus.Data.DbContexts;
-public class IcarusDbContext:DbContext
+public class IcarusDbContext : DbContext
 {
-    public IcarusDbContext(DbContextOptions<IcarusDbContext> options):base(options)
+    public IcarusDbContext(DbContextOptions<IcarusDbContext> options) : base(options)
     {
     }
 
@@ -13,4 +16,17 @@ public class IcarusDbContext:DbContext
     public DbSet<Category> Categories { get; set; }
     public DbSet<Department> Departments { get; set; }
     public DbSet<DepartmentCategory> DepartmentCategories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        SeedData(modelBuilder);
+    }
+    private static void SeedData(ModelBuilder modelBuilder)
+    {
+        UserSeedData.SeedDataUsers(modelBuilder);
+        AssetSeedData.SeedDataAssets(modelBuilder);
+        DepartamentCategorySeedData.SeedDataDepartamentCategory(modelBuilder);
+    }
 }
