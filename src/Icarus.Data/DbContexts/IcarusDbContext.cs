aec3d2a5d2ba9814601bc90dd90fce8ced4d78d0
dkar1mov03 <dkarimov049@gmail.com>
2023-12-23 15:59:13 +0500

    Problem Solved

--- src/Icarus.Data/DbContexts/IcarusDbContext.cs
+++ src/Icarus.Data/DbContexts/IcarusDbContext.cs
@@ -1,7 +1,4 @@
-﻿using Icarus.Data.DbContexts.SeedDatas.AssetSeedDatas;
-using Icarus.Data.DbContexts.SeedDatas.DepartmentCategorySeedDatas;
-using Icarus.Data.DbContexts.SeedDatas.UserSeedDatas;
-using Icarus.Domain.Entities;
+﻿using Icarus.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Icarus.Data.DbContexts;
@@ -13,37 +10,39 @@ public class IcarusDbContext : DbContext
 
     public DbSet<User> Users { get; set; }
     public DbSet<Asset> Assets { get; set; }
+    public DbSet<Request> Requests { get; set; }
     public DbSet<Category> Categories { get; set; }
     public DbSet<Department> Departments { get; set; }
+    public DbSet<DepartmentResponse> DepartmentResponses { get; set; }
     public DbSet<DepartmentCategory> DepartmentCategories { get; set; }
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
+    //protected override void OnModelCreating(ModelBuilder modelBuilder)
+    //{
+    //    base.OnModelCreating(modelBuilder);
 
-        // Category - DepartmentCategory Configuration
+    //    // Category - DepartmentCategory Configuration
 
-        modelBuilder.Entity<Category>()
-            .HasMany(c => c.DepartmentCategories)
-            .WithOne(dc => dc.Category)
-            .HasForeignKey(dc => dc.CategoryId)
-            .OnDelete(DeleteBehavior.Cascade);
+    //    //modelBuilder.Entity<Category>()
+    //    //    .HasMany(c => c.DepartmentCategories)
+    //    //    .WithOne(dc => dc.Category)
+    //    //    .HasForeignKey(dc => dc.CategoryId)
+    //    //    .OnDelete(DeleteBehavior.Cascade);
 
-        // Department - DepartmentCategory Configuration
+    //    //// Department - DepartmentCategory Configuration
 
-        modelBuilder.Entity<Department>()
-            .HasMany(d => d.DepartmentCategories)
-            .WithOne(dc => dc.Department)
-            .HasForeignKey(dc => dc.DepartmentId)
-            .OnDelete(DeleteBehavior.Cascade);
+    //    //modelBuilder.Entity<Department>()
+    //    //    .HasMany(d => d.DepartmentCategories)
+    //    //    .WithOne(dc => dc.Department)
+    //    //    .HasForeignKey(dc => dc.DepartmentId)
+    //    //    .OnDelete(DeleteBehavior.Cascade);
 
-           SeedData(modelBuilder);
-    }
+    //       SeedData(modelBuilder);
+    //}
 
-    private static void SeedData(ModelBuilder modelBuilder)
-    {
-        UserSeedData.SeedDataUsers(modelBuilder);
-        AssetSeetData.SeedDataAssets(modelBuilder);
-        DepartamentCategorySeedData.SeedDataDepartamentCategory(modelBuilder);
-    }
+    //private static void SeedData(ModelBuilder modelBuilder)
+    //{
+    //    UserSeedData.SeedDataUsers(modelBuilder);
+    //    AssetSeetData.SeedDataAssets(modelBuilder);
+    //    DepartamentCategorySeedData.SeedDataDepartamentCategory(modelBuilder);
+    //}
 }
